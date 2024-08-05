1a7a1f08a839b170074d4e3254649f526567aae5
zeeshan666 <zeeshanmanzoor.zm@gmail.com>
2024-08-05 18:17:03 +0500

    minor change

--- server.js
+++ server.js
@@ -32,7 +32,10 @@ require("./app/routes/auth.routes")(app);
 
 // connect to db
 mongoose
-  .connect(process.env.MONGO_URI)
+  .connect(process.env.MONGO_URI,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
   .then(() => {
     // listen for requests
     app.listen(process.env.PORT, () => {
