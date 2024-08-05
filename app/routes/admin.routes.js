b4ca7812dd600bd1f7469dcce4cb91c029d39807
zeeshan666 <zeeshanmanzoor.zm@gmail.com>
2024-08-05 19:12:33 +0500

    minor fix

--- app/routes/admin.routes.js
+++ app/routes/admin.routes.js
@@ -9,25 +9,25 @@ const adminController = require("../controllers/admin.controller");
   'full_name',
 ];
 
-module.exports = function(router) {
+module.exports = function(app) {
 
 
   // Get Users Routes
-  router.post("/api/get-all-sellers", [authJwt.verifyToken, authJwt.isAdmin], adminController.getAllSellers);
-  router.post("/api/get-all-buyers", [authJwt.verifyToken, authJwt.isAdmin], adminController.getAllBuyers);
-  router.get("/api/get-user/:id", [authJwt.verifyToken, authJwt.isAdmin], adminController.getUser);
-  router.patch("/api/update-user/:id", [validationMiddleware(updateUserRequiredFields), authJwt.verifyToken, authJwt.isAdmin], adminController.updateUser);
-  router.patch("/api/toggle-user-status/:id", [authJwt.verifyToken, authJwt.isAdmin], adminController.updateUserStatus);
+  app.post("/api/get-all-sellers", [authJwt.verifyToken, authJwt.isAdmin], adminController.getAllSellers);
+  app.post("/api/get-all-buyers", [authJwt.verifyToken, authJwt.isAdmin], adminController.getAllBuyers);
+  app.get("/api/get-user/:id", [authJwt.verifyToken, authJwt.isAdmin], adminController.getUser);
+  app.patch("/api/update-user/:id", [validationMiddleware(updateUserRequiredFields), authJwt.verifyToken, authJwt.isAdmin], adminController.updateUser);
+  app.patch("/api/toggle-user-status/:id", [authJwt.verifyToken, authJwt.isAdmin], adminController.updateUserStatus);
 
   // Assets Api
-  router.post('/api/create-asset', [authJwt.verifyToken, authJwt.isAdmin, upload.array('media')], adminController.createAsset);
-  router.post('/api/get-all-assets', [authJwt.verifyToken, authJwt.isAdmin], adminController.getAllAssets);
-  router.get('/api/get-asset/:id', [authJwt.verifyToken, authJwt.isAdmin], adminController.getAsset);
-  router.post('/api/get-assets-by-creator/:creator_id', [authJwt.verifyToken, authJwt.isAdmin], adminController.getAssetsByCreator);
-  router.delete('/api/delete-asset-media/:asset_id/:media_id', [authJwt.verifyToken, authJwt.isAdmin], adminController.deleteMediaAssets);
-  router.patch('/api/update-asset/:id', [authJwt.verifyToken, authJwt.isAdmin, upload.array('media')], adminController.updateAsset);
-  router.patch("/api/toggle-asset-status/:id", [authJwt.verifyToken, authJwt.isAdmin], adminController.updateAssetStatus);
-  router.post('/api/sellers-with-asset-count', [authJwt.verifyToken, authJwt.isAdmin], adminController.getSellersWithAssetCount);
-  router.get('/api/get-all-keywords', [authJwt.verifyToken, authJwt.isAdmin], adminController.getAllKeywords);
+  app.post('/api/create-asset', [authJwt.verifyToken, authJwt.isAdmin, upload.array('media')], adminController.createAsset);
+  app.post('/api/get-all-assets', [authJwt.verifyToken, authJwt.isAdmin], adminController.getAllAssets);
+  app.get('/api/get-asset/:id', [authJwt.verifyToken, authJwt.isAdmin], adminController.getAsset);
+  app.post('/api/get-assets-by-creator/:creator_id', [authJwt.verifyToken, authJwt.isAdmin], adminController.getAssetsByCreator);
+  app.delete('/api/delete-asset-media/:asset_id/:media_id', [authJwt.verifyToken, authJwt.isAdmin], adminController.deleteMediaAssets);
+  app.patch('/api/update-asset/:id', [authJwt.verifyToken, authJwt.isAdmin, upload.array('media')], adminController.updateAsset);
+  app.patch("/api/toggle-asset-status/:id", [authJwt.verifyToken, authJwt.isAdmin], adminController.updateAssetStatus);
+  app.post('/api/sellers-with-asset-count', [authJwt.verifyToken, authJwt.isAdmin], adminController.getSellersWithAssetCount);
+  app.get('/api/get-all-keywords', [authJwt.verifyToken, authJwt.isAdmin], adminController.getAllKeywords);
 
 };
